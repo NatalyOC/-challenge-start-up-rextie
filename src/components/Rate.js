f98d15cacc2e617c45c162ce3c5b96bbcd0b9562
NatalyOC <noteroc@hotmail.com>
2018-03-31 02:51:28 -0500

    agregando constantes

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {rateContants} from '../constants';
 
 const Rate =()=>(
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-12">
+        <h5>Tipo de Cambio: <span class="badge badge-secondary">Compra: {rateContants.RATE_BUY}</span> <span class="badge badge-secondary">Venta:{rateContants.RATE_SALE}</span></h5>
+      </div>
+    </div>
+  </div>
 
-  
 )
+export default Rate;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const rateContants ={
+  RATE_BUY: 3.2460,
+  RATE_SALE: 3.2840,
+
+};
