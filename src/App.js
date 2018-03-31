60cd01c15b8a98b856b7a3604c4e3a2cfb31c84e
NatalyOC <noteroc@hotmail.com>
2018-03-31 03:24:08 -0500

    implementando componente Exchange

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Rate from './components/Rate';
+import Exchange from './containers/Exchange';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Rate/>
+        <Exchange/>
       </div>
     );
   }
---
+++ src/containers/Exchange.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Exchange =()=>(
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-12">
+        <div>
+          <label>Importe:</label>
+          <input type="text" placeholder="Importe"/>
+        </div>
+      </div>
+    </div>
+  </div>
+
+)
+
+export default Exchange;
