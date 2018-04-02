dbce73b09d8b3387bdcd67ad1ece5b66a0be820c
NatalyOC <noteroc@hotmail.com>
2018-04-01 23:39:49 -0500

    modificando clases

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -5,7 +5,7 @@ const Rate =()=>(
   <div className="container">
     <div className="row">
       <div className="col-lg-12">
-        <h5>Tipo de Cambio: <span class="badge badge-secondary">Compra: {rateContants.RATE_BUY}</span> <span class="badge badge-secondary">Venta:{rateContants.RATE_SALE}</span></h5>
+        <h5>Tipo de Cambio: <span className="badge badge-secondary">Compra: {rateContants.RATE_BUY}</span> <span className="badge badge-secondary">Venta:{rateContants.RATE_SALE}</span></h5>
       </div>
     </div>
   </div>
